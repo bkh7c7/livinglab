53f84fa7b9ead4d283b6da99ae5af686af919e3c
bkh7c7 <bkh7c7@gmail.com>
2020-12-11 08:54:26 +0900

    update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testRouter = require('./routes/testAdd');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.get('/testPage', testRouter.testPage);
+console.log(__dirname+app);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
